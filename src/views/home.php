e373e6602d9709a8525b0cef72be0c69fef9812e
Billy M. M. R <engineer@haryono.co.id>
2017-02-21 09:59:46 +0700

    cff

--- src/views/home.php
+++ src/views/home.php
@@ -2,16 +2,21 @@
 <html>
 <head>
 	<title>Json Mocker</title>
+	<style type="text/css">
+		body {
+			font-family : "Arial", Helvetica, sans-serif;
+		}
+	</style>
 </head>
 <body>
-Welcome to Json Mocker
+<h1>Welcome to Json Mocker</h1>
 <ul>
 	<li><a href="/db">db</a></li>
-<?php //var_dump($this->data);
-foreach ($this->data as $key => $value) {
-	echo '<li><a href="/'.$key.'">'.$key.'</a></li>';
-}
-?>
+	<?php
+	foreach ($this->data as $key => $value) {
+		echo '<li><a href="/'.$key.'">'.$key.'</a></li>';
+	}
+	?>
 </ul>
 </body>
 </html>
