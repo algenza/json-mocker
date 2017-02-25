19a03a99288fe466afa172562fa1d04685291d1d
Billy M. M. R <engineer@haryono.co.id>
2017-02-25 11:41:21 +0700

    add post method to add new json object record

--- src/Resolver.php
+++ src/Resolver.php
@@ -45,12 +45,12 @@ class Resolver
 		return self::notFound($response);		
 	}
 	private static function handleGet(Request $request, Response $response, ServiceProvider $service){
-		if($request->uri() == '/'){
+		if($request->pathname() == '/'){
 			$service->data = self::processJson();
 			return self::render($service,'home');
 		}
 
-		$uriPart = self::extracUri($request->uri());
+		$uriPart = self::extracUri($request->pathname());
 
 		if($uriPart[0]=='db'){
 			$output = self::processJson();
@@ -58,15 +58,45 @@ class Resolver
 		}
 
 		if(self::isValidUri($uriPart)){
-			return self::takeData($response, $uriPart);
+			return self::takeData($response, $uriPart, $request->paramsGet());
 		}
 
 		return self::notFound($response);		
 	}
 	private static function handlePost(Request $request, Response $response, ServiceProvider $service){
-		$obj = new \stdClass();
-		$obj->message = "You hit post method";
-		return $response->json($obj);		
+		if($request->pathname() == '/'){
+			return self::notFound($response);
+		}
+		$uriPart = self::extracUri($request->pathname());
+		if(self::isValidUri($uriPart, true)){
+			return self::saveData($response, $uriPart, $request->body());
+		}
+
+		return self::notFound($response);		
+	}
+	private static function saveData($response, $uriPart, $params){
+		$fullFile = json_decode(file_get_contents(self::$targetJson));
+		$maxid = 1;
+		if(isset($fullFile->{$uriPart[0]})){
+			foreach ($fullFile->{$uriPart[0]} as $item) {
+				if($item->id > $maxid){
+					$maxid = $item->id;
+				}
+			}			
+		}
+		$newdata = new \stdClass();;
+		$newdata->id = (int)$maxid+1;
+		$data = json_decode($params);
+		if($data->id){
+			unset($data->id);
+		}
+		foreach ($data as $key => $value) {
+			$newdata->{$key} = $value; 
+		}
+		$fullFile->{$uriPart[0]}[] = $newdata;
+		file_put_contents(self::$targetJson,json_encode($fullFile));
+
+		return $response->json($newdata);		
 	}
 	private static function handlePut(Request $request, Response $response, ServiceProvider $service){
 		$obj = new \stdClass();
@@ -90,7 +120,6 @@ class Resolver
 		$obj->message = 'Page Not Found';
 		return $response->json($obj);
 	}
-
 	private static function extracUri($uri)
 	{
 		$uriPart = substr($uri,1);
@@ -98,30 +127,52 @@ class Resolver
 		return $uriPart;
 	}
 
-	private static function isValidUri($uriPart)
+	private static function isValidUri($uriPart, $forPost = false)
 	{
 		$fullFile = json_decode(file_get_contents(self::$targetJson));
 		if(!isset($fullFile->{$uriPart[0]})){
 			return false;
 		}
-
-		if(isset($uriPart[1])){
-			foreach ($fullFile->{$uriPart[0]} as $item) {
-				if($item->id == $uriPart[1]){
-					return true;
+		if (!$forPost) {
+			if(isset($uriPart[1])){
+				foreach ($fullFile->{$uriPart[0]} as $item) {
+					if($item->id == $uriPart[1]){
+						return true;
+					}
 				}
+				return false;
 			}
-			return false;
+		} else {
+			if(isset($uriPart[1])){
+				return false;
+			}
 		}
 
 		return true;
 	}
 
-	private static function takeData($response, $uriPart)
+	private static function takeData($response, $uriPart, $params = null)
 	{
 		$fullFile = json_decode(file_get_contents(self::$targetJson));
 		if(!isset($uriPart[1])){
-			return $response->json($fullFile->{$uriPart[0]});
+			if(!$params){
+				return $response->json($fullFile->{$uriPart[0]});
+			}
+
+			$filteredData = array_filter($fullFile->{$uriPart[0]}, function ($obj) use ($params){
+				foreach ($params as $key => $value) {
+					if(isset($obj->{$key})){
+						if(strpos(strtolower($obj->{$key}),strtolower($value))===false){
+							return false;
+						}
+					}else{
+						return false;
+					}
+				}
+				return true;
+			});
+
+			return $response->json($filteredData);
 		}
 
 		foreach ($fullFile->{$uriPart[0]} as $item) {
