6e73bdccccf84613d4ecc8e2f3a1433211ac4e82
Billy M. M. R <engineer@haryono.co.id>
2017-02-27 13:13:18 +0700

    change config data path

--- src/Config.php
+++ src/Config.php
@@ -2,7 +2,7 @@
 
 return [
 
-	'schemaPath'	=>	__DIR__.'/../api/schema.json',
-	'targetJson'	=>	__DIR__.'/../api/db.json',
+	'schemaPath'	=>	__DIR__.'/../data/schema.json',
+	'targetJson'	=>	__DIR__.'/../data/db.json',
 	'viewPath'	=>	__DIR__.'/views/'
 ];
